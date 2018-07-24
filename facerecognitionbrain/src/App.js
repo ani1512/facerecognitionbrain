4ae4c958b3540d88181fd8295a12e9db704a3aad
ani1512 <reachanish1512@gmail.com>
2018-07-25 03:56:32 +0530

    Done with the skeleton... A little tweak with particles required

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import 'tachyons';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        value_area: 1000
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Particles className='particles'
+              params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />    
+        {/*
+        
+        
+        <FaceRecognition />*/}
       </div>
     );
   }
